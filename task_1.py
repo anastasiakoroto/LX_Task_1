686aaae54df52e805d12ff824b68caa9e9266ff4
anastasiakoroto <anastasiakoroto@gmail.com>
2020-02-08 17:32:56 +0300

    Change structure of class Union for convinitent use, rename Union to ListsHandler

--- task_1.py
+++ task_1.py
@@ -21,16 +21,29 @@ class FileHandler:
 class FileWriter:
 
     def write_to_json(self, obj):
-        with open('format.json', 'w') as json_file:
+        with open('updated_rooms.json', 'w') as json_file:
             json.dump(obj, json_file)
 
-    def write_to_xml(self):
+    def write_to_xml(self, obj):
         pass
 
 
-class Union:
+class ListsHandler:
 
-    def add_student_to_room(self, room, student):
+    def __init__(self, st_path, room_path, format_type):
+        self.st_path = st_path
+        self.room_path = room_path
+        self.format = format_type
+
+        st_file_handler = FileHandler(self.st_path)
+        self.students_list = st_file_handler.decode_json()
+
+        rooms_file_handler = FileHandler(self.room_path)
+        self.rooms_list = rooms_file_handler.decode_json()
+
+        self.file_writer = FileWriter()
+
+    def _add_student_to_room(self, room, student):
         student = student.copy()
         student.pop('room')
         if room.get('students') is None:
@@ -39,30 +52,25 @@ class Union:
             room['students'].append(student)
         return room
 
-    def get_rooms_and_students_list(self, stud_list, room_list):
-        room_list = copy.deepcopy(room_list)
-        for student in stud_list:
+    def get_rooms_and_students_list(self):
+        room_list = copy.deepcopy(self.rooms_list)
+        for student in self.students_list:
             for room in room_list:
                 if student['room'] == room['id']:
-                    self.add_student_to_room(room, student)
+                    self._add_student_to_room(room, student)
         return room_list
+
+    def write_updated_rooms_to_file(self):
+        updated_room_list = self.get_rooms_and_students_list()
+        if self.format == 'json':
+            self.file_writer.write_to_json(updated_room_list)
+        elif self.format == 'xml':
+            self.file_writer.write_to_xml(updated_room_list)
 
 
 if __name__ == '__main__':
     students = '/Users/Anastasia/PycharmProjects/LeverX Course/task_1/students.json'
     rooms = '/Users/Anastasia/PycharmProjects/LeverX Course/task_1/rooms.json'
 
-    students_file = FileHandler(students)
-    students_list = students_file.decode_json()
-    print('Students list: ', students_list)
-    rooms_file = FileHandler(rooms)
-    rooms_list = rooms_file.decode_json()
-    print('Rooms list: ', rooms_list)
-
-    union_obj = Union()
-    result = union_obj.get_rooms_and_students_list(students_list, rooms_list)
-    print('-' * 50)
-    print('Result:', result, sep='\n')
-
-    fw = FileWriter()
-    fw.write_to_json(result)
+    obj = ListsHandler(students, rooms, 'json')
+    obj.write_updated_rooms_to_file()
